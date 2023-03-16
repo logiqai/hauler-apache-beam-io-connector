cd8fe640277f03dff2c9e0ddd3577ea389b78e0e
Kevin D'souza <kevin.d@logiq.ai>
2023-03-16 18:12:33 +0530

    event: enable manipulation of Event properties by builder pattern
    Signed-off-by: Kevin D'souza <kevin.d@logiq.ai>

--- lib/src/main/kotlin/logiqio/LogiqEvent.kt
+++ lib/src/main/kotlin/logiqio/LogiqEvent.kt
@@ -5,24 +5,59 @@ import java.io.Serializable
 
 data class LogiqEvent(
     @SerializedName("namespace")
-    val namespace: String? = "",
+    private var namespace: String? = "",
 
     @SerializedName("message")
-    val message: String? = "",
+    private var message: String? = "",
 
     @SerializedName("@timestamp")
-    val timestamp: Int? = 0,
+    private var timestamp: Int? = 0,
 
     @SerializedName("host")
-    val host: String? = "",
+    private var host: String? = "",
 
     @SerializedName("proc_id")
-    val pid: String? = "",
+    private var pid: String? = "",
 
     @SerializedName("app_name")
-    val appName: String? = "",
+    private var appName: String? = "",
 
     @SerializedName("cluster_id")
-    val clusterId: String? = "",
-) : Serializable
+    private var clusterId: String? = "",
+) : Serializable {
+    fun withNamespace(namespace: String?) : LogiqEvent {
+        this.namespace = namespace
+        return this
+    }
+
+    fun withMessage(message: String?): LogiqEvent {
+        this.message = message
+        return this
+    }
+
+    fun withTimestamp(timestamp: Int?) : LogiqEvent {
+        this.timestamp = timestamp
+        return this
+    }
+
+    fun withHost(host: String?) : LogiqEvent {
+        this.host = host
+        return this
+    }
+
+    fun withPid(pid: String?) : LogiqEvent {
+       this.pid = pid
+       return this
+    }
+
+    fun withAppName(appName: String?) : LogiqEvent {
+        this.appName = appName
+        return this
+    }
+
+    fun withClusterId(clusterId: String?) : LogiqEvent {
+        this.clusterId = clusterId
+        return this
+    }
+}
 
